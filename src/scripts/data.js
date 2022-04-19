f823a12c2cc7c7bb2c947eff511dbdcb8aee52b4
aliveGUY <ilyadubrivvin@gmail.com>
2022-04-19 15:38:32 +0300

    Shorten code

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,6 +1,4 @@
 const form = document.forms[0];
-const title = form.elements[0];
-const author = form.elements[1];
 const button = form.elements.add;
 const dltBtn = document.querySelectorAll('.Delete-btn');
 let data = JSON.parse(localStorage.getItem('Data'));
@@ -10,19 +8,20 @@ if (data === null) {
 }
 
 const book = { title: '', Author: '' };
-button.addEventListener('click', () => {
+form.addEventListener('input', (e) => {
   const temp = Object(book);
-  temp.title = title.value;
-  temp.Author = author.value;
-  data.push(temp);
-  localStorage.setItem('Data', JSON.stringify(data));
-  document.location.reload();
+  temp[e.target.id] = form.elements[e.target.id].value;
+  button.onclick = () => {
+    data.push(temp);
+    localStorage.setItem('Data', JSON.stringify(data));
+    document.location.reload();
+  };
 });
 
 dltBtn.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
+  btn.onclick = (e) => {
     data.splice(e.target.parentNode.id, 1);
     localStorage.setItem('Data', JSON.stringify(data));
     document.location.reload();
-  });
+  };
 });
