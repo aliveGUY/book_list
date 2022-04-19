71c674992721429f4a2203c9e2726f6f0744331d
aliveGUY <ilyadubrivvin@gmail.com>
2022-04-19 23:39:48 +0300

    edit data.js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,27 +1,21 @@
+import Methods from './book.js';
+
 const form = document.forms[0];
 const button = form.elements.add;
 const dltBtn = document.querySelectorAll('.Delete-btn');
-let data = JSON.parse(localStorage.getItem('Data'));
+const book = new Methods();
 
-if (data === null) {
-  data = [];
-}
-
-const book = { title: '', Author: '' };
 form.addEventListener('input', (e) => {
-  const temp = Object(book);
-  temp[e.target.id] = form.elements[e.target.id].value;
+  book[e.target.id] = form.elements[e.target.id].value;
   button.onclick = () => {
-    data.push(temp);
-    localStorage.setItem('Data', JSON.stringify(data));
-    document.location.reload();
+    book.upload();
   };
 });
 
 dltBtn.forEach((btn) => {
   btn.onclick = (e) => {
-    data.splice(e.target.parentNode.id, 1);
-    localStorage.setItem('Data', JSON.stringify(data));
-    document.location.reload();
+    book.id = e.target.parentNode.id;
+    book.delete();
   };
 });
+
