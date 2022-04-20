1a88bf8736ca8d9bcadd8708a8597d955740788e
aliveGUY <ilyadubrivvin@gmail.com>
2022-04-20 19:38:01 +0300

    Fix linter errors

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,27 +1,31 @@
-const nav = document.querySelectorAll('.Navigation-item')
-const add_book = document.querySelector('.add-book')
-const book_list = document.querySelector('.book-list');
-const contact = document.querySelector('.contact')
+const nav = document.querySelectorAll('.Navigation-item');
+const addbook = document.querySelector('.add-book');
+const booklist = document.querySelector('.book-list');
+const contact = document.querySelector('.contact');
 
 nav.forEach((item) => {
-    item.onclick = (e) => {
-
-        switch(item.firstElementChild.innerHTML) {
-            case 'List':
-                book_list.style.display = 'block'
-                add_book.style.display = 'none';
-                contact.style.display = 'none';
-                break;
-            case 'Add new':
-                add_book.style.display = "flex";
-                book_list.style.display = 'none'
-                contact.style.display = 'none';
-                break;
-            case 'Contant':
-                contact.style.display = 'block';
-                book_list.style.display = 'none'
-                add_book.style.display = 'none';
-                break;
-        }
+  item.onclick = () => {
+    switch (item.firstElementChild.innerHTML) {
+      case 'List':
+        booklist.style.display = 'block';
+        addbook.style.display = 'none';
+        contact.style.display = 'none';
+        break;
+      case 'Add new':
+        addbook.style.display = 'flex';
+        booklist.style.display = 'none';
+        contact.style.display = 'none';
+        break;
+      case 'Contant':
+        contact.style.display = 'block';
+        booklist.style.display = 'none';
+        addbook.style.display = 'none';
+        break;
+      default:
+        booklist.style.display = 'block';
+        addbook.style.display = 'none';
+        contact.style.display = 'none';
+        break;
     }
-})
+  };
+});
